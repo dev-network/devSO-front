62705f1be9d5cac1037ab8e4449227ed36ac7399
qqqkyj <48308120+qqqkyj@users.noreply.github.com>
2025-12-31 16:34:52 +0900

    팀원 모집 관련 RootLayout, router 수정(비로그인 사용자도 조회 가능) (#46)

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -10,7 +10,7 @@ import ChatListPage from "../pages/ChatListPage.jsx";
 import ChatRoomPage from "../pages/ChatRoomPage.jsx";
 import ProfileEditPage from "../pages/ProfileEditPage.jsx";
 import ProfilePage from "../pages/ProfilePage.jsx";
-import ProfilePostPage from "../pages/ProfilePostPage.jsx"; // 새로 추가된 페이지
+import ProfilePostPage from "../pages/ProfilePostPage.jsx";
 import RecruitMainPage from "../pages/RecruitMainPage.jsx";
 import RecruitCreatePage from "../pages/RecruitCreatePage.jsx";
 import RecruitDetailPage from "../pages/RecruitDetailPage.jsx";
@@ -19,118 +19,118 @@ import { AuthProvider } from "../contexts/AuthContext.jsx";
 import { ChatProvider } from "../contexts/ChatContext.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <AuthProvider>
-        <ChatProvider>
-          <RootLayout />
-        </ChatProvider>
-      </AuthProvider>
-    ),
-    children: [
-      {
-        index: true,
-        element: <RecentPostListPage />,
-      },
-      {
-        path: "posts",
-        element: <RecentPostListPage />,
-      },
-      {
-        path: "posts/new",
-        element: (
-          <PrivateRoutes>
-            <PostCreatePage />
-          </PrivateRoutes>
-        ),
-      },
-      {
-        path: "posts/:id/edit",
-        element: (
-          <PrivateRoutes>
-            <PostCreatePage />
-          </PrivateRoutes>
-        ),
-      },
-      {
-        path: "posts/:id",
-        element: <PostDetailPage />,
-      },
-      {
-        path: "login",
-        element: (
-          <PublicRoute>
-            <LoginPage />
-          </PublicRoute>
-        ),
-      },
-      {
-        path: "signup",
-        element: (
-          <PublicRoute>
-            <SignupPage />
-          </PublicRoute>
-        ),
-      },
-      {
-        path: "oauth/kakao/callback",
-        element: <KakaoCallback />,
-      },
-      {
-        path: "recruits",
-        element: <RecruitMainPage />,
-      },
-      {
-        path: "recruits/create",
-        element: <RecruitCreatePage />,
-      },
-      {
-        path: "recruits/:id",
-        element: <RecruitDetailPage />,
-      },
-      {
-        // 인증이 필요한 보호된 루트들
-        element: <PrivateRoutes />,
-        children: [
-          {
-            path: "chat",
-            element: <ChatListPage />,
-          },
-          {
-            path: "chat/:roomId",
-            element: <ChatRoomPage />,
-          },
-          {
-            // profile 관련 경로를 하나의 부모로 묶어 중첩 라우팅 적용
-            path: "profile",
-            children: [
-              {
-                index: true, // /profile (내 프로필)
-                element: <ProfilePage />,
-              },
-              {
-                path: "edit", // /profile/edit (프로필 수정)
-                element: <ProfileEditPage />,
-              },
-              {
-                path: "my/posts", // /profile/my/posts (내 작성글 목록)
-                element: <ProfilePostPage />,
-              },
-              {
-                path: ":username", // /profile/:username (타인 프로필)
-                element: <ProfilePage />,
-              },
-              {
-                path: ":username/posts", // /profile/:username/posts (타인 작성글 목록)
-                element: <ProfilePostPage />,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: (
+			<AuthProvider>
+				<ChatProvider>
+					<RootLayout />
+				</ChatProvider>
+			</AuthProvider>
+		),
+		children: [
+			{
+				index: true,
+				element: <RecentPostListPage />,
+			},
+			{
+				path: "posts",
+				element: <RecentPostListPage />,
+			},
+			{
+				path: "posts/new",
+				element: (
+					<PrivateRoutes>
+						<PostCreatePage />
+					</PrivateRoutes>
+				),
+			},
+			{
+				path: "posts/:id/edit",
+				element: (
+					<PrivateRoutes>
+						<PostCreatePage />
+					</PrivateRoutes>
+				),
+			},
+			{
+				path: "posts/:id",
+				element: <PostDetailPage />,
+			},
+			{
+				path: "login",
+				element: (
+					<PublicRoute>
+						<LoginPage />
+					</PublicRoute>
+				),
+			},
+			{
+				path: "signup",
+				element: (
+					<PublicRoute>
+						<SignupPage />
+					</PublicRoute>
+				),
+			},
+			{
+				path: "oauth/kakao/callback",
+				element: <KakaoCallback />,
+			},
+			// 누구나 볼 수 있는 모집글 경로
+			{
+				path: "recruits",
+				element: <RecruitMainPage />,
+			},
+			{
+				path: "recruits/:id",
+				element: <RecruitDetailPage />,
+			},
+			{
+				// 인증이 필요한 보호된 루트들
+				element: <PrivateRoutes />,
+				children: [
+					{
+						path: "recruits/create", // 작성 페이지를 보호 구역으로 이동
+						element: <RecruitCreatePage />,
+					},
+					{
+						path: "chat",
+						element: <ChatListPage />,
+					},
+					{
+						path: "chat/:roomId",
+						element: <ChatRoomPage />,
+					},
+					{
+						path: "profile",
+						children: [
+							{
+								index: true,
+								element: <ProfilePage />,
+							},
+							{
+								path: "edit",
+								element: <ProfileEditPage />,
+							},
+							{
+								path: "my/posts",
+								element: <ProfilePostPage />,
+							},
+							{
+								path: ":username",
+								element: <ProfilePage />,
+							},
+							{
+								path: ":username/posts",
+								element: <ProfilePostPage />,
+							},
+						],
+					},
+				],
+			},
+		],
+	},
 ]);
 
-export default router; 
+export default router;
