9600f18372296da3ace4ff04975809c8cd85082b
jintheog <jintheog11@gmail.com>
2026-01-06 09:47:58 +0900

    feed search

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -2,7 +2,9 @@ import { getFeed } from "../api";
 import PostGridPage from "./PostGridPage.jsx";
 
 const FeedPage = () => {
-  return <PostGridPage title="피드" fetcher={getFeed} emptyText="피드에 표시할 게시글이 없습니다." />;
+  return (
+    <PostGridPage title="피드" fetcher={getFeed} emptyText="피드에 표시할 게시글이 없습니다." enableSearch />
+  );
 };
 
 export default FeedPage;
--- src/pages/RecentPostListPage.jsx
+++ src/pages/RecentPostListPage.jsx
@@ -2,7 +2,7 @@ import { getPosts } from "../api";
 import PostGridPage from "./PostGridPage.jsx";
 
 const RecentPostListPage = () => {
-  return <PostGridPage title="최신 게시글" fetcher={getPosts} />;
+  return <PostGridPage title="최신 게시글" fetcher={getPosts} enableSearch />;
 };
 
 export default RecentPostListPage;
