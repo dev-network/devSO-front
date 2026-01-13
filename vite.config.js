5308d29d7391ff84168f1be02b7fa2ba2a800895
Qfactor <qfactor2013@gmail.com>
2026-01-13 14:59:20 +0900

    modifying config add loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+  define: {
+      'process.env.MY_SYSTEM_VAR': JSON.stringify(env.VITE_API_URL),
+    },
   plugins: [react(), tailwindcss()],
+  }
 })
