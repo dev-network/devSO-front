16b43e63a9c28d679b563c370d2f75a0e1bde97c
Qfactor <qfactor2013@gmail.com>
2026-01-21 11:49:46 +0900

    remove vite.config.js define config and add .env value VITE_API_URL

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the current mode (development, production, etc.)
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   return {
-  define: {
-      'process.env.MY_SYSTEM_VAR': JSON.stringify(env.VITE_API_URL),
-    },
+  // define: {
+  //     'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+  //   },
   plugins: [react(), tailwindcss()],
   }
 })
